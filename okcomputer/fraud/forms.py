f86412ca198c19e158acdd2c3a0e2587fa870ff8
confusedaf <aswathys2k@gmail.com>
2020-12-19 13:54:25 +0530

    second commit

--- okcomputer/fraud/forms.py
+++ okcomputer/fraud/forms.py
@@ -5,13 +5,14 @@ from .models import UserProfileInfo,Transaction
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
+    password.widget.attrs['class']='form-control'
     class Meta:
         model = User
         fields = {'username' , 'email' , 'password'}
         widgets = {
-            'username' : forms.TextInput(attrs = {'placeholder': 'Username'}),
-            'email'    : forms.TextInput(attrs = {'placeholder': 'E-Mail'}),
-            'password' : forms.PasswordInput(attrs = {'placeholder': 'Password'}),
+            'username' : forms.TextInput(attrs = {'placeholder': 'Username', 'class':'form-control'}),
+            'email'    : forms.TextInput(attrs = {'placeholder': 'E-Mail', 'class':'form-control'}),
+            'password' : forms.PasswordInput(attrs = {'placeholder': 'Password', 'class':'form-control'}),
         }
 
 
@@ -19,7 +20,7 @@ class UserExtra(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
         fields = {'profile_pic'}
-        
+
 
 
 class Transaction_Details(forms.ModelForm):
